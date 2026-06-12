727471f3157c1bb73fbd418cdf64c5b0c8b62991
sharva paralkar <sharva.paralkar@gmail.com>
2026-06-12 10:33:40 -0500

    new

--- comp-arch-course/src/pages/index.js
+++ comp-arch-course/src/pages/index.js
@@ -139,7 +139,7 @@ export default function Home() {
               color: 'var(--cal-ink, #1a1a1a)',
               margin: 0,
             }}>
-              CAL • Rhino, Grasshopper, & Structural Analysis
+              CAL • Analyzing Structures
             </h1>
 
             {/* Rule */}
